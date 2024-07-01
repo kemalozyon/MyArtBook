282fcb6110d87d7d00e41fb455924b81cf62f701
Kemal Özyön <kemalozyon51@gmail.com>
2024-07-01 08:20:51 +0300

    Initial Commit

---
+++ SaveImagesToCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaveImagesToCoreData
+//
+//  Created by Kemal Özyön on 1.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
