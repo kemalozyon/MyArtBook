a758fc643f89cf4f37dde4076cf4d21058c8d0e7
Kemal Özyön <kemalozyon51@gmail.com>
2024-07-01 12:34:44 +0300

    Adding variables to Core Data finished

--- SaveImagesToCoreData/DetailsViewController.swift
+++ SaveImagesToCoreData/DetailsViewController.swift
@@ -6,25 +6,30 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var namefield: UITextField!
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var artistField: UITextField!
-    
     @IBOutlet weak var yearField: UITextField!
+    @IBOutlet weak var buttonProperty: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+        buttonProperty.isEnabled = false
+        //Use imageView as button in order to add photos from library or camera to your imageview.
         imageView.isUserInteractionEnabled = true
         let imageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(addImage))
         imageView.addGestureRecognizer(imageGestureRecognizer)
         //This code to close keyboard when u click on view so that u can reach the save button
         let viewGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(viewGestureRecognizer)
+        
     }
     
     @objc func hideKeyboard(){
@@ -54,8 +59,32 @@ class DetailsViewController: UIViewController, UINavigationControllerDelegate, U
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.editedImage] as? UIImage
         self.dismiss(animated: true,completion: nil)
+        //Button shows when you choose the image
+        buttonProperty.isEnabled = true
+        
     }
     @IBAction func saveButtonClicked(_ sender: Any) {
+        if let year = Int(yearField.text!){
+            //
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context  = appDelegate.persistentContainer.viewContext
+            
+            let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+            
+            //Adding objects to DataBase
+            newPainting.setValue(namefield.text!, forKey: "name")
+            newPainting.setValue(artistField.text!, forKey: "artist")
+            newPainting.setValue(UUID(), forKey: "id")
+            let myPaint = imageView.image!.jpegData(compressionQuality: 0.5)
+            newPainting.setValue(myPaint, forKey: "image")
+            newPainting.setValue(year, forKey: "year")
+        }else{
+            let alert = UIAlertController(title: "Error", message: "Please enter a number for the blank of age!", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.destructive, handler: nil)
+            alert.addAction(okButton)
+            present(alert,animated: true)
+        }
+        
     }
     
 }
